0920160ce50f0ca9b5737ffe5601d016dadb5ea6
Владислав Парийский <pariyskiy.va@softsps.ru>
2021-11-15 17:24:55 +0300

    AdminPageLogin

---
+++ RaifProjects/AdminLoginPageTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaifProjects
+{
+    public class AdminLoginPageTests
+    {
+        public IWebDriver? Driver;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            StartTestBrowser();
+        }
+
+        [TearDown]
+        protected void TearDown()
+        {
+            Driver?.Quit();
+        }
+
+
+        private void StartTestBrowser()
+        {
+
+            Driver = new FirefoxDriver();
+            Driver.Manage().Window.Maximize();
+
+        }
+
+        [Test]
+        public void OpenAdminPage()
+        {
+            Driver?.Navigate().GoToUrl(" http://localhost/litecart/admin/");
+            var username = Driver?.FindElement(By.Name ("username"));
+            username?.Clear();
+            username?.SendKeys("admin");
+            var password = Driver?.FindElement(By.Name("password"));
+            password?.Clear();
+            password?.SendKeys("admin");
+            var logButon = Driver?.FindElement(By.Name("login"));
+            logButon?.Click();
+        }
+    }
+}
